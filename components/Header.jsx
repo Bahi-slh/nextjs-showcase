4989cdb705f3a540fc5c3945c37ca5ea6f068957
bahi slh <slh.bahare@gmail.com>
2024-06-25 15:19:47 +1000

    Create Header component

---
+++ components/Header.jsx
@@ -0,0 +1,7 @@
+export default function Header() {
+    return (
+      <header className="bg-gray-100 p-4 mb-6">
+        <h1 className="text-2xl font-bold text-gray-800 text-center">Aenean suscipit sapien et erat</h1>
+      </header>
+    );
+  }
